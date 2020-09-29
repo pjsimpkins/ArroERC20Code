dce819d7b6b647b2faea8f50d9588f5429b18ff5
Oliver Garcia <ogarci5@gmail.com>
2020-09-29 13:29:48 -0500

    Upgrade to use OpenZeppelin SDK

--- contracts/ArroToken.sol
+++ contracts/ArroToken.sol
@@ -1,8 +1,9 @@
+// SPDX-License-Identifier: MIT
 /**
  * Source Code first verified at https://etherscan.io on Monday, July 30, 2018
  (UTC) */
 
-pragma solidity ^0.7.2;
+pragma solidity ^0.6.2;
 
 // ----------------------------------------------------------------------------
 // 'ARRO' token contract
@@ -17,134 +18,33 @@ pragma solidity ^0.7.2;
 //
 // ----------------------------------------------------------------------------
 
-import { ERC20Interface } from "./ERC20Interface.sol";
-import { SafeMath } from "./SafeMath.sol";
-import { Owned } from "./Owned.sol";
-import { ApproveAndCallFallBack } from "./ApproveAndCallFallBack.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/access/Ownable.sol";
 
-// ----------------------------------------------------------------------------
-// ERC20 Token, with the addition of symbol, name and decimals and assisted
-// token transfers
-// ----------------------------------------------------------------------------
-contract ArroToken is ERC20Interface, Owned, SafeMath {
-    string public symbol;
-    string public  name;
-    uint8 public decimals;
-    uint public _totalSupply;
+contract ArroToken is Initializable, ERC20UpgradeSafe, OwnableUpgradeSafe {
 
-    mapping(address => uint) balances;
-    mapping(address => mapping(address => uint)) allowed;
-
+    uint256 private _totalSupply;
 
     // ------------------------------------------------------------------------
-    // Constructor
+    // Gives Owner all tokens
     // ------------------------------------------------------------------------
-    constructor() public {
-        symbol = "ARRO";
-        name = "Arro.io";
-        decimals = 18;
-        _totalSupply = 30000000000000000000000000000;
-        balances[0xb27A875e4b3b5DcE9e7BE9De258EDCc40BeF09F1] = _totalSupply;
-        emit Transfer(address(0), 0xb27A875e4b3b5DcE9e7BE9De258EDCc40BeF09F1, _totalSupply);
+    function initialize(address owner) public {
+        // Defaults to 18 decimals
+        __ERC20_init("Arro.io", "ARRO");
+        __Ownable_init();
+
+        _totalSupply = 30000000000 * (10 ** 18);
+
+        _mint(owner, 10000 * (10 ** uint256(decimals())));
+        transferOwnership(owner);
     }
-
-
-    // ------------------------------------------------------------------------
-    // Total supply
-    // ------------------------------------------------------------------------
-    function totalSupply() public constant returns (uint) {
-        return _totalSupply  - balances[address(0)];
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Get the token balance for account tokenOwner
-    // ------------------------------------------------------------------------
-    function balanceOf(address tokenOwner) public constant returns (uint balance) {
-        return balances[tokenOwner];
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Transfer the balance from token owner's account to to account
-    // - Owner's account must have sufficient balance to transfer
-    // - 0 value transfers are allowed
-    // ------------------------------------------------------------------------
-    function transfer(address to, uint tokens) public returns (bool success) {
-        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
-        balances[to] = safeAdd(balances[to], tokens);
-        emit Transfer(msg.sender, to, tokens);
-        return true;
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Token owner can approve for spender to transferFrom(...) tokens
-    // from the token owner's account
-    //
-    // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
-    // recommends that there are no checks for the approval double-spend attack
-    // as this should be implemented in user interfaces
-    // ------------------------------------------------------------------------
-    function approve(address spender, uint tokens) public returns (bool success) {
-        allowed[msg.sender][spender] = tokens;
-        emit Approval(msg.sender, spender, tokens);
-        return true;
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Transfer tokens from the from account to the to account
-    //
-    // The calling account must already have sufficient tokens approve(...)-d
-    // for spending from the from account and
-    // - From account must have sufficient balance to transfer
-    // - Spender must have sufficient allowance to transfer
-    // - 0 value transfers are allowed
-    // ------------------------------------------------------------------------
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-        balances[from] = safeSub(balances[from], tokens);
-        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
-        balances[to] = safeAdd(balances[to], tokens);
-        emit Transfer(from, to, tokens);
-        return true;
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Returns the amount of tokens approved by the owner that can be
-    // transferred to the spender's account
-    // ------------------------------------------------------------------------
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
-        return allowed[tokenOwner][spender];
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Token owner can approve for spender to transferFrom(...) tokens
-    // from the token owner's account. The spender contract function
-    // receiveApproval(...) is then executed
-    // ------------------------------------------------------------------------
-    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
-        allowed[msg.sender][spender] = tokens;
-        emit Approval(msg.sender, spender, tokens);
-        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
-        return true;
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Don't accept ETH
-    // ------------------------------------------------------------------------
-    function () public payable {
-        revert();
-    }
-
 
     // ------------------------------------------------------------------------
     // Owner can transfer out any accidentally sent ERC20 tokens
     // ------------------------------------------------------------------------
-    function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
-        return ERC20Interface(tokenAddress).transfer(owner, tokens);
+    function transferAnyERC20Token(address tokenAddress, uint amount) public onlyOwner returns (bool) {
+        _transfer(tokenAddress, owner(), amount);
+        return true;
     }
+
 }
